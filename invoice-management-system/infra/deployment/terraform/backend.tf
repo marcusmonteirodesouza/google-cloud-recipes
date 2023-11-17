245a44f59c93635a49a1404d861139fb36c0e97a
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-11-16 23:13:31 -0500

    Update backend.tf

--- invoice-management-system/infra/deployment/terraform/backend.tf
+++ invoice-management-system/infra/deployment/terraform/backend.tf
@@ -1,5 +1,5 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "88656c94-8d66-e4e7-9a9a-4c7bf4027a80"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "e419e88a-2519-b8ab-9b34-2bf658c45b13"
+  }
+}
